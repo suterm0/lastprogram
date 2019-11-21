24265732a594cbcd1153296473d2c252682843d9
Michael Suter <suterm@hartwick.edu>
2019-11-21 18:48:37 -0500

    work

--- apply.py
+++ apply.py
@@ -10,13 +10,15 @@ titles.append(title)
 first = input("Enter the authors first name.>>")
 last = input("Enter the authors last name.>>")
 age = input("Enter the authors age.>>")
+person = (first, last)
+author = (first, last, age)
 edition = input("Enter the Edition of the book.>>")
 number = input("Enter the book number.>>")
 publisher = input("Enter the publisher.>>")
 year_published = input("Enter the year the book was published.>>")
 quantity = input("How many books are there to buy?>>")
 price = input("How much does the book cost?>>")
-first_book = Textbook(title, first, last, age, edition, number, publisher, year_published, quantity, price)
+first_book = Textbook(title, first, last, age, author, edition, number, publisher, year_published, quantity, price)
 
 
 def menu():
@@ -24,7 +26,6 @@ def menu():
     while h == 2:
         answer = input("Would you like to add a book to the library?(Y/N).>>")
         if answer == 'y' and 'Y':
-
             print(f"Here is your first books details:"
                   f"{first_book}.")
         elif answer == 'n' and 'N':
@@ -33,7 +34,5 @@ def menu():
             print("Invalid response. Please answer with (Y/N).")
 
 
-Person.personal_description()
+Textbook.description()
 menu()
-
-
