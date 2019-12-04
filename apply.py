8f456b4d2895a0c0811d9c7821c83352f15cf879
Michael Suter <suterm@hartwick.edu>
2019-12-04 08:14:54 -0500

    ugh

--- apply.py
+++ apply.py
@@ -52,22 +52,22 @@ def menu():
         answer2 = input("Would you like to add a book to the library?(Y/N).>>")
         if answer2 == 'y' and 'Y':
             title3 = input("What is the title of the book?>>")
-        titles.append(title3)
-        first2 = input("Enter the authors first name.>>")
-        last2 = input("Enter the authors last name.>>")
-        age2 = input("Enter the authors age.>>")
-        person2 = Person.personal_description
-        author2 = (first2, last2, age2)
-        edition2 = input("Enter the Edition of the book.>>")
-        number2 = input("Enter the book number.>>")
-        publisher2 = input("Enter the publisher.>>")
-        year_published2 = input("Enter the year the book was published.>>")
-        quantity2 = int(input("How many books are there to buy?>>"))
-        price2 = int(input("How much does the book cost?>>"))
-        total = quantity + quantity2
-        second_book = Textbook(title3, first2, last2, age2, edition2, number2, publisher2, year_published2, quantity2, price2)
-        print(f"There are now two different books in stock, There is {quantity} {title}'s and there is {quantity2} of {title3}.")
-        print(f"There is a total of {total} books in inventory right now")
+            titles.append(title3)
+            first2 = input("Enter the authors first name.>>")
+            last2 = input("Enter the authors last name.>>")
+            age2 = input("Enter the authors age.>>")
+            person2 = Person.personal_description
+            author2 = (first2, last2, age2)
+            edition2 = input("Enter the Edition of the book.>>")
+            number2 = input("Enter the book number.>>")
+            publisher2 = input("Enter the publisher.>>")
+            year_published2 = input("Enter the year the book was published.>>")
+            quantity2 = int(input("How many books are there to buy?>>"))
+            price2 = int(input("How much does the book cost?>>"))
+            total = quantity + quantity2
+            second_book = Textbook(title3, first2, last2, age2, edition2, number2, publisher2, year_published2, quantity2, price2)
+            print(f"There are now  different books in stock, There is {quantity} {title}'s and there is {quantity2} of {title3}.")
+            print(f"There is a total of {total} books in inventory right now")
 
 
 first_book.description()
